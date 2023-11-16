81f76166b2ed9e39a902287190fd39921705626c
jasonbuchanan145 <83380304+jasonbuchanan145@users.noreply.github.com>
2023-11-15 20:39:23 -0600

    Cache (#8)
    * cache
    
    * cache

--- services/spark/src/main/java/edu/usd/hpc/SparkSession.java
+++ services/spark/src/main/java/edu/usd/hpc/SparkSession.java
@@ -1,7 +1,23 @@
 package edu.usd.hpc;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableCaching
 public class SparkSession {
+    @Bean(name = "dataset")
+    public Dataset<Row> dataset() {
+        org.apache.spark.sql.SparkSession sparkSession = org.apache.spark.sql.SparkSession.builder().master("local[*]").appName("hpc").getOrCreate();
+        return sparkSession.read().format("jdbc")
+                .format("jdbc")
+                .option("driver", "com.mysql.cj.jdbc.Driver")
+                .option("url", "jdbc:mysql://localhost:3307/HPC")
+                .option("dbtable", "airlineAirportData")
+                .option("user", "root")
+                .option("password", "root").load();
+    }
 }
